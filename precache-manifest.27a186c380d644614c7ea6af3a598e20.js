e391ec7e6d26804b0f59f0a37834107c5375f3d1
kgagracz <krystian.gagracz@gmail.com>
2020-01-26 16:47:32 +0100

    Updates

---
+++ precache-manifest.27a186c380d644614c7ea6af3a598e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed42cb9a4c516b15e646f0eacf4c2396",
+    "url": "/bticoin-rate-app/index.html"
+  },
+  {
+    "revision": "bb11f6df014d4136d79e",
+    "url": "/bticoin-rate-app/static/css/main.01d7fa3f.chunk.css"
+  },
+  {
+    "revision": "d53a0baae8b3db97334a",
+    "url": "/bticoin-rate-app/static/js/2.a8f63be2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bticoin-rate-app/static/js/2.a8f63be2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb11f6df014d4136d79e",
+    "url": "/bticoin-rate-app/static/js/main.953ac9aa.chunk.js"
+  },
+  {
+    "revision": "4e95fae13eb6a64612c7",
+    "url": "/bticoin-rate-app/static/js/runtime-main.06feb5e2.js"
+  }
+]);
